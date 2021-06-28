641faadead9e2f4e9bd872113d768f490775c957
Isaac <isaac@unl.edu>
2021-06-28 11:46:47 -0500

    Event lock

---
+++ js/tabs-experiment/lock.js
@@ -0,0 +1,26 @@
+﻿define([], function () {
+
+	var _export = function() {
+		var _lock = false;
+
+		this.getLock = function () {
+			if (!_lock) {
+				console.log('Success: Lock Acquired');
+				_lock = true;
+				return true;
+			};
+			console.log('Fail: Already Locked');
+			return false;
+		};
+
+		this.unlock = function () {
+			_lock = false;
+		};
+
+		this.isLocked = function () {
+			return _lock;
+		};
+	};
+
+	return _export;
+});
